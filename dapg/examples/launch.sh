368e1533f81e4957849e402df0fc32d63e7c54c9
quantumiracle <zhding@mail.ustc.edu.cn>
2022-08-26 13:12:32 -0400

    test on server

--- dapg/examples/launch.sh
+++ dapg/examples/launch.sh
@@ -2,4 +2,8 @@ DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 mkdir -p log/$DATE
 
-nohup python job_script.py --output rl_scratch_exp --config rl_scratch.txt  --record_video True --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/rl_scratch.log &
+declare -a tasks=('rl_scratch' 'bcrl' 'dapg')
+
+for i in ${!tasks[@]}; do
+    nohup python job_script.py --output ${tasks[$i]}_exp --config ${tasks[$i]}.txt  --record_video True --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/${tasks[$i]}.log &
+done
