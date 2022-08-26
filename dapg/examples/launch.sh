292da64c8459fc08ef90ce2539bdc80e23633c96
quantumiracle <zhding@mail.ustc.edu.cn>
2022-08-26 11:00:44 -0400

    fix launch

--- dapg/examples/launch.sh
+++ dapg/examples/launch.sh
@@ -2,4 +2,4 @@ DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 mkdir -p log/$DATE
 
-nohup job_script.py --output rl_scratch_exp --config rl_scratch.txt  --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/rl_scratch.log &
+nohup python job_script.py --output rl_scratch_exp --config rl_scratch.txt  --record_video True --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/rl_scratch.log &
