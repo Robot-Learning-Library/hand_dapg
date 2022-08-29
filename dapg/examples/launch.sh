6e1a15ab8292c5f5c1800831c9bb09f2f3d32d65
quantumiracle <zhding@mail.ustc.edu.cn>
2022-08-29 11:50:15 -0400

    change sh

--- dapg/examples/launch.sh
+++ dapg/examples/launch.sh
@@ -5,5 +5,5 @@ mkdir -p log/$DATE
 declare -a tasks=('rl_scratch' 'bcrl' 'dapg')
 
 for i in ${!tasks[@]}; do
-    nohup python job_script.py --output ${tasks[$i]}_exp --config ${tasks[$i]}.txt  --record_video True --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/${tasks[$i]}.log &
+    nohup python job_script.py --output results/$DATE/${tasks[$i]}_exp --config ${tasks[$i]}.txt  --record_video True --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/${tasks[$i]}.log &
 done
