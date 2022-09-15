027dd8aa51e1926f5672418383a27ffb7d170152
quantumiracle <zhding@mail.ustc.edu.cn>
2022-09-15 15:46:29 -0400

    collect data

---
+++ dapg/examples/collect_data.sh
@@ -0,0 +1,15 @@
+DATE=`date '+%Y%m%d_%H%M'`
+echo "Save as: " $DATE
+mkdir -p log/$DATE
+mkdir -p results/$DATE
+# declare -a methods=('rl_scratch' 'bcrl' 'dapg' 'npg_discrim')
+declare -a methods=('rl_scratch')
+declare -a envs=('relocate' 'pen' 'hammer' 'door')
+# declare -a envs=('pen' 'hammer' 'door')
+
+
+for i in ${!methods[@]}; do
+    for j in ${!envs[@]}; do
+        nohup python -W ignore collect_data.py --output collect_data --config cfg/${methods[$i]}_${envs[$j]}.txt  --record_video True --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/${methods[$i]}_${envs[$j]}.log &
+    done
+done
