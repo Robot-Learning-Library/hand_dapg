069b5c2402bb89e4f138a191a57c2bed17e4451e
zihan <zhding@mail.ustc.edu.cn>
2022-09-20 22:20:22 -0400

    fix

--- dapg/examples/train_discrim.sh
+++ dapg/examples/train_discrim.sh
@@ -2,8 +2,5 @@ DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 mkdir -p log/$DATE
 
-for i in ${!methods[@]}; do
-    for j in ${!envs[@]}; do
-        nohup python -W ignore train_discrim.py --itr 100000 --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/discriminator.log &
-    done
-done
+echo nohup python -W ignore train_discrim.py --itr 100000 --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/discriminator.log
+nohup python -W ignore train_discrim.py --itr 100000 --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/discriminator.log &
