c26a54d1bdfc25c34680921a5bd642190610d602
quantumiracle <zhding@mail.ustc.edu.cn>
2022-10-02 14:03:21 -0400

    fix

--- dapg/examples/train_discrim_all.sh
+++ dapg/examples/train_discrim_all.sh
@@ -5,6 +5,6 @@ mkdir -p log/$DATE
 declare -a envs=('relocate-v0' 'pen-v0' 'door-v0' 'hammer-v0')
 
 for i in ${!envs[@]}; do
-    echo nohup python -W ignore train_discrim.py --leave_one_out ${envs[$i]} --itr 10000 --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/discriminator.log
-    nohup python -W ignore train_discrim.py --leave_one_out ${envs[$i]} --itr 10000 --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/discriminator.log &
+    echo nohup python -W ignore train_discrim.py --leave_one_out ${envs[$i]} --itr 10000 --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/discriminator_no_${envs[$i]}.log
+    nohup python -W ignore train_discrim.py --leave_one_out ${envs[$i]} --itr 10000 --save_id $DATE --wandb_activate True --wandb_entity quantumiracle >> log/$DATE/discriminator_no_${envs[$i]}.log &
 done
